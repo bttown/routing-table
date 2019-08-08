7cb7fe6ea746f2803a56fbc4c02ab349dfd1d277
importcjj <importcjj@gmail.com>
2019-08-08 14:53:16 +0800

    log

--- table.go
+++ table.go
@@ -306,7 +306,7 @@ func (table *Table) add(n *Contact) {
 		return
 	}
 
-	log.Print("\rhandle new node", n.UDPAddr)
+	log.Println("\033[2Ahandle new node", n.UDPAddr)
 	bkt := table.buckets[bktid]
 	original, bumped := bkt.bump(n)
 	switch {
